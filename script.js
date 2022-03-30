a0da776dad9eb525b2a8f85590458bd68ac5ca8e
kacperoqq <sierantkacper9@gmail.com>
2022-03-30 23:28:34 +0200

    Dodaliśmy gówno działający kod

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("kuba pedzio");
+const express = require("express"); 
+const app = express();
+app.set("view engine", "ejs");
+// app.set("views","./views");
+app.get('/',function(req,res){
+    res.send("<h1>siema</h1>");
+});
+
+app.listen(3000);
