8379a823e4702e42ad7e494d93fefcf14f58d59a
Vrrrum <kamil2006wojtek@gmail.com>
2022-05-06 17:51:57 +0200

    Pierwszy prawdziwy commit, trochę baz danych i struktura projektu

--- script.js
+++
@@ -1,9 +0,0 @@
-const express = require("express"); 
-const app = express();
-app.set("view engine", "ejs");
-// app.set("views","./views");
-app.get('/',function(req,res){
-    res.send("<h1>siema</h1>");
-});
-
-app.listen(3000);
