415fb32774c5586f519b416dc080182d8661f043
Vrrrum <kamil2006wojtek@gmail.com>
2022-06-05 19:48:41 +0200

    Repertuar jest pobierany z bazy danych i wyświetla się poprawnie

--- getSchedule.js
+++ getSchedule.js
@@ -1,12 +1,19 @@
 const Schedule = require("./models/Schedule");
+const Movies = require("./models/Movies");
 
 const getSchedule = async (city, date) => {
   try {
-    const day = new Date(date).getDay;
-    const schedule = await Schedule.find({ city: city, dayOfWeek: day });
+    const scheduleDate = new Date(date);
+    const day = scheduleDate.getDay();
+    const schedule = await Schedule.findOne({
+      city: city,
+      dayOfWeek: day,
+    })
+      .populate("movies.movieId")
+      .exec();
     return schedule;
   } catch (e) {
-    console.error(e.message);
+    console.error(e);
   }
 };
 
